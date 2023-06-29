c0c14f560ce5c94ee6c5f92d74d735661764b1e8
sahilmoktan <moktanshail9@gmail.com>
2023-06-29 23:59:23 +0545

    understand global style, style change on click

--- bblog/app/about/aboutStudent/page.js
+++ bblog/app/about/aboutStudent/page.js
@@ -1,11 +1,16 @@
+'use client'
 // import React from 'react'
 import Link from 'next/link'
+import { useState } from 'react'
 
 
 function AboutStudent() {
+  const [h3style, setH3styel] = useState({backgroundColor:'green'})
   return (
     <div>
-        <h2 className='heading'>About Student Page</h2>
+        <h2 className='heading' style={{color:'skyblue'}}>About Student Page</h2>
+        <h3 style={h3style}>head 3</h3>
+        <button onClick={()=> setH3styel({backgroundColor:'red'})}> update style</button>
       <br/>
       <br/>
       <Link href="/">Go to Home</Link>
