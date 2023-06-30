9f0d6bcda89666b26d61922d931fbf78f8d1b984
sahilmoktan <moktanshail9@gmail.com>
2023-06-30 12:08:34 +0545

    image optimization, next.config setup

--- bblog/app/page.js
+++ bblog/app/page.js
@@ -3,6 +3,8 @@ import { useState } from 'react'
 import styles from './page.module.css'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
+import Image from 'next/image'
+import profile from '../public/vercel.svg'
 
 export default function Home() {
 
@@ -21,6 +23,13 @@ export default function Home() {
       <Link href="/login">Go to login</Link>
       <Link href="/about">Go to about</Link>
       <button onClick={()=>router.push('/login')}>Go to login Page</button>
+      <Image
+      src={profile}/>
+      <Image
+      src="https://images.unsplash.com/photo-1687800132770-8f1600a5849e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=928&q=80"
+      width={500}
+      height={500}
+      />
     </main>
   )
 }
