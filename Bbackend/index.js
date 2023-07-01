ba7e8287e1a896700496a595dc24b806ef5ac472
sahilmoktan <moktanshail9@gmail.com>
2023-07-01 14:22:09 +0545

    basic backend established but in ES6 module

---
+++ Bbackend/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from 'dotenv'
+// import multer from "multer";
+import helmet from 'helmet';
+import morgan from "morgan";
+import {connectDb} from "./config/dbConnection.js"
+
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({policy:'cross-origin'}))
+app.use(morgan("common"))
+app.use(bodyParser.json({limit:'30mb', extended:'true'}))
+app.use(bodyParser.urlencoded({limit:'30mb', extended:'true'}))
+app.use(cors())
+
+connectDb();
+
+const port = process.env.PORT || 6001;
+
+
+// app.use("/api/contacts", require("./routes/contactRoutes"));
+// app.use("/api/users", require("./routes/userRoutes"));
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
