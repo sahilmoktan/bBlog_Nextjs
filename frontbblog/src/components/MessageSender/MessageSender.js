2d070162184e63d24de9fdcd3cb6581f378e0cc3
sahilmoktan <moktanshail9@gmail.com>
2023-07-28 22:18:53 +0545

    message post section

--- frontbblog/src/components/MessageSender/MessageSender.js
+++ frontbblog/src/components/MessageSender/MessageSender.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./messageSender.css";
 import { Avatar } from "@material-ui/core";
+import VideocamIcon from '@mui/icons-material/Videocam';
+import PhotoLibraryIcon from '@mui/icons-material/PhotoLibrary';
+import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
 
 export default function MessageSender() {
   return (
@@ -12,12 +15,23 @@ export default function MessageSender() {
             className="messageSender_input"
             placeholder={`What's on your mind`}
           />
-
-          
         </form>
       </div>
 
-      <div className="messageSender_bottom"></div>
+      <div className="messageSender_bottom">
+        <div className="messageSender_option">
+        <VideocamIcon style={{color:'red'}}/>
+        <h4>Live Video</h4>
+        </div>
+        <div className="messageSender_option">
+        <PhotoLibraryIcon style={{color:'green'}}/>
+        <h4>Photo/Video</h4>
+        </div>
+        <div className="messageSender_option">
+        <InsertEmoticonIcon style={{color:'orange'}}/>
+        <h4>Feeling/Acativity</h4>
+        </div>
+      </div>
     </div>
   );
 }
