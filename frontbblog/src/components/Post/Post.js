c2c6d3ec23fc7abf957f3cb73ffca527b7a9b062
sahilmoktan <moktanshail9@gmail.com>
2023-07-29 22:33:46 +0545

    post section looks okey

--- frontbblog/src/components/Post/Post.js
+++ frontbblog/src/components/Post/Post.js
@@ -1,11 +1,29 @@
 import React from 'react'
 import './post.css'
+import { Avatar } from '@material-ui/core'
 
 
 export default function Post({profilePic, image, username, timestamp, message}) {
   return (
     <div className='post'>
-        Post
+        <div className="post_top">
+            <Avatar src={profilePic} className='post_avatar'/>
+        <div className="post_topInfo">
+            <h4>{username}</h4>
+            <p>Timestam ....</p>
+        </div>
+        </div>
+        
+        
+        
+        <div className="post_bottom">
+            <p>{message}</p>
+        </div>
+        
+        <div className="post_image">
+            <img src={image} alt='' />
+        </div>
+        
 
     </div>
   )
--- frontbblog/src/components/feed/Feed.js
+++ frontbblog/src/components/feed/Feed.js
@@ -9,7 +9,14 @@ export default function Feed() {
     <div className="feed">
       <StoryReel  />
       <MessageSender/>
-      <Post/>
+      <Post
+      profilePic="https://sp-images.summitpost.org/947006.jpg?auto=format&fit=max&ixlib=php-2.1.1&q=35&w=1024&s=d877834568578388ef13b78e3cd7ba2b"
+      message="Wow this works"      
+      timestamp="It's timestamp"
+      username="Soung"
+      image="https://th.bing.com/th/id/OIP.RhhTNA_3b7FEwe03T3D3SQHaEK?pid=ImgDet&w=1366&h=768&rs=1"
+      
+      />
       <Post/>
       <Post/>
       
